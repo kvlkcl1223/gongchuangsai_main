4e1201e45117cdeb180fd6d7cba07cd3f3fe0978
kvlkcl1223 <2720477355@qq.com>
2024-12-03 15:00:17 +0800

    a22

--- fonts.py
+++ fonts.py
@@ -1,5 +1,12 @@
-import matplotlib.font_manager
+# import matplotlib.font_manager
+#
+# # 获取所有可用的字体
+# font_paths = matplotlib.font_manager.findSystemFonts()
+# print(font_paths)
 
-# 获取所有可用的字体
-font_paths = matplotlib.font_manager.findSystemFonts()
-print(font_paths)
+import tkinter as tk
+import tkinter.font as tkfont
+
+root = tk.Tk()
+font = tkfont.nametofont(root.option_get('font', 'TkDefaultFont'))
+print(font.actual())  # 打印当前字体信息
