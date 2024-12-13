d024ff2db04c41a8fbebc7f6beb4befa91b9e5aa
kvlkcl1223 <2720477355@qq.com>
2024-12-13 23:08:51 +0800

    12.13

--- uart.py
+++ uart.py
@@ -1,6 +1,31 @@
 import serial
 import time
 
+def uart_transition(com, ser_ttyAMA4):
+    serial_cnt = 1  # 调用一次该程序
+
+    while ser_ttyAMA4.in_waiting > 0:
+        ser_ttyAMA4.read(ser_ttyAMA4.in_waiting)  # 读取并丢弃所有数据
+    ser_ttyAMA4.reset_input_buffer()
+
+    while ser_ttyAMA4.in_waiting == 0:
+        ser_ttyAMA4.flushInput()
+        ser_ttyAMA4.write(com)
+        print("发送的数据", com)
+        time.sleep(0.02)
+        serial_cnt += 1
+
+        if serial_cnt > 5:
+            break
+    try:
+        while ser_ttyAMA4.in_waiting > 0:
+            data_to_discard = ser_ttyAMA4.read()
+            print("data_to_discard", data_to_discard)
+
+    except Exception as e:
+        # 如果解码失败，处理异常
+        print("error")
+
 def open_serial(port, baudrate, timeout=None, retry_interval=1):
     """
     尝试打开串口，直到成功为止。
@@ -30,12 +55,16 @@ ser.reset_input_buffer()
 
 while True:
     time.sleep(0.1)
+
     if ser.in_waiting > 0:  # 检查是否有数据等待读取
 
         # 读取一行数据并解码
         try:
             received_data = ser.readline().decode('ascii').strip()
             print("received_data", received_data)
+            time.sleep(0.1)
+            uart_transition("Tar=q4!",ser)
+            time.sleep(8)
         except UnicodeDecodeError:
             # 如果解码失败，处理异常
             # queue_transmit.put("Tar=repeat!")
